00e08ebeb04f33fae660c524fe622c96591e218d
wushanglin <123@qq.com>
2023-12-25 17:35:10 +0800

    fix some BUG

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const obj = {
   web_password: "123456",
   jwtSecret: "accesstokey",
   server_port: 4090,
-  openai_url: 'https://youProxyUrl.com',
+  ninjia_url: 'https://youProxyUrl.com',
   db_host: '127.0.0.1',
   db_user: 'accesstokey',
   db_password: '123456',
